1919cc37b55060c43c44cb6723c728624230c098
neoslvh <luuvanhung171105@gmail.com>
2025-07-16 04:35:21 +0700

    Bưóc 4

--- src/PersonalTaskManagerViolations.java
+++ src/PersonalTaskManagerViolations.java
@@ -155,4 +155,16 @@ public class PersonalTaskManagerViolations {
         }
         return dueDate;
     }
+
+    private boolean checkDuplicate(JSONArray tasks, String title, LocalDate dueDate) {
+            for (Object obj : tasks) {
+                JSONObject existing = (JSONObject) obj;
+                if (existing.get("title").toString().equalsIgnoreCase(title) &&
+                    existing.get("due_date").toString().equals(dueDate.format(DATE_FORMATTER))) {
+                    System.out.println(String.format("Lỗi: Nhiệm vụ '%s' đã tồn tại với cùng ngày đến hạn.", title));
+                    return true;
+                }
+            }
+        return false;
+    }
 }
